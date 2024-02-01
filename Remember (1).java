5805a4f950af9aef84e11ec1e720d66725e05e98
derpysschoolaccount <150294967+derpysschoolaccount@users.noreply.github.com>
2024-02-01 09:59:31 -0800

    Add files via upload

---
+++ Remember (1).java
@@ -0,0 +1,19 @@
+/****************************
+* Project: typinglab1.jpx
+* Programmer: Zack Rothe
+* Date: 2/1/24
+* Program: Remember.java
+**************/
+
+public class Remember
+{
+    public static void main(String[] args) {
+        System.out.println("Things I need to remember about Java:");
+        System.out.println("1. Quotes come in pairs.");
+        System.out.println("2. Project names and program names are different");
+        System.out.println("3. Documentation is expected for each program");
+        System.out.println("4. Code is formatted in a clear, readable manner");
+        System.out.println("5. Double click on an error to find its location.");
+        System.out.println("6. Programming is a skill, but it's fun");
+    }
+}
