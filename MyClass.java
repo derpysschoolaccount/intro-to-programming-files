bee71255f820d245fdc3a01deeb8701369a5b2f5
derpysschoolaccount <150294967+derpysschoolaccount@users.noreply.github.com>
2024-02-01 10:03:50 -0800

    Add files via upload

---
+++ MyClass.java
@@ -0,0 +1,13 @@
+public class MyClass {
+    public static void main(String args[]) {
+      System.out.println("I have a spelling checker.");
+      System.out.println("It came with my pea sea.");
+      System.out.println("It plane lee marks four my revue.");
+      System.out.println("Miss steaks aye can knot sea.\n");
+      
+      System.out.println("Eye ran this poem threw it.");
+      System.out.println("Your sure reel glad two no.");
+      System.out.println("Its vary polished in its weigh,");
+      System.out.println("My checker told me sew");
+    }
+}
