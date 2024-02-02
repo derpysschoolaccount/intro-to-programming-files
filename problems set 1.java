a03e6091dded1893d9fe601e09411d49a022a3bf
derpysschoolaccount <150294967+derpysschoolaccount@users.noreply.github.com>
2024-02-02 09:50:56 -0800

    Create problems set 1.java

---
+++ problems set 1.java
@@ -0,0 +1,11 @@
+public class MyClass {
+    public static void main(String args[]) {
+      System.out.println("\n\n\n\n\n\n\t\t\t\tOHS\n"); // school initials
+      
+      
+      System.out.println("*************************************\n*  Sue\'s Swimwear                   *      \n*  SwimWithSue@swimwear.com         *\n*************************************\n"); // sue ad
+      
+     System.out.println("\nPoem: something about cheese, i can\'t remember what it was\nProverb: always bring your towel");
+      
+    }
+}
