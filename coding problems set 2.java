be614e5c1f0c43ea36f46a2ca56f51ea0315f2ad
derpysschoolaccount <150294967+derpysschoolaccount@users.noreply.github.com>
2024-02-05 09:37:21 -0800

    Create coding problems set 2.java

---
+++ coding problems set 2.java
@@ -0,0 +1,12 @@
+public class MyClass {
+    public static void main(String args[]) {
+        // 1:
+      System.out.println("\t\tDog Show Winners\nPlace:\t1\t2\t3\t4\t5\t6\nDog #:\t76\t32\t18\t47\t11\t29\n"); // prints dog show winners
+      
+      // 2:
+      System.out.println("Q\"yz89\"Ja4\"tp\n");
+      
+      // 3:
+      System.out.println("Anderson\t3452\nBoyton\t\t5344\nCarrington\t3677\nHouston\t\t5988\nMcDougal\t5233\nRobertson\t3623\nStryker\t\t5112");
+    }
+}
