e196f6eecfaed58b1a93c8b37bd0683909d41cb1
derpysschoolaccount <150294967+derpysschoolaccount@users.noreply.github.com>
2024-02-07 09:34:44 -0800

    Create making a program or something idk what it's called.java

---
+++ making a program or something idk what it's called.java
@@ -0,0 +1,9 @@
+public class MyClass {
+    public static void main(String args[]) {
+        // 1:
+      System.out.println("NAME\tSCORE\nPicard\t1874\nRiker\t1789"); // names or something idk
+      
+      // 2:
+      System.out.println("\n\n\n\n\n\n\t\t\t*\t*\t*\t*\t*\n\t\t\t\t\t*\n\t\t\t\t\t*\n\t\t\t\t\t*"); // T thing
+    }
+}
